252d5b320718aab3adca950e9c9d01456ea70919
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-14 19:59:59 +0900

    feat: 투입 금액 예외 처리 기능 추가

---
+++ src/main/java/vendingmachine/UserMoney.java
@@ -0,0 +1,12 @@
+package vendingmachine;
+
+import static vendingmachine.ValidationUtils.*;
+
+public class UserMoney {
+
+	public static void valid(String UserInputMoney) {
+		int userMoney = validNumberFormat(UserInputMoney);
+		validUnitMoney(userMoney);
+		isPositive(userMoney);
+	}
+}
---
+++ src/test/java/vendingmachine/UserMoneyTest.java
@@ -0,0 +1,22 @@
+package vendingmachine;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserMoneyTest {
+	@Test
+	@DisplayName("투입 금액 예외 처리")
+	void validTest() {
+		String UserInputMoney = "5";
+		String UserInputMoney2 = "-1";
+		String UserInputMoney3 = "a1";
+		assertThatThrownBy(() -> {
+			UserMoney.valid(UserInputMoney);
+			UserMoney.valid(UserInputMoney2);
+			UserMoney.valid(UserInputMoney3);
+		}).isInstanceOf(IllegalArgumentException.class);
+
+	}
+}
