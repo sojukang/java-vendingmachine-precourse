813f1890e01e7ddf7567634a168cecebc8da2a9a
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-17 10:28:54 +0900

    feat: 상품 정보 파싱 기능 추가

--- src/main/java/vendingmachine/Messages.java
+++ src/main/java/vendingmachine/Messages.java
@@ -4,4 +4,5 @@ public class Messages {
 	public static final String ERROR_NOT_INT = "[ERROR] 금액은 숫자여야 합니다.";
 	public static final String ERROR_INVALID_UNIT_NO = "[ERROR] 금액은 10원 단위어야 합니다.";
 	public static final String ERROR_SMALLER_THAN_MIN = "[ERROR] 금액이 최소 금액보다 작습니다.";
+	public static final String ERROR_INVALID_ITEM = "[ERROR] 잘못된 상품 입력입니다.";
 }
---
+++ src/main/java/vendingmachine/Parser.java
@@ -0,0 +1,26 @@
+package vendingmachine;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Parser {
+	public static String[] splitBySemicolon(String input) {
+		return input.split(";");
+	}
+
+	public static String removeBracket(String input) {
+		if (input.charAt(0) != '[' || input.charAt(input.length() - 1) != ']') {
+			throw new IllegalArgumentException(Messages.ERROR_INVALID_ITEM);
+		}
+		return input.substring(1, input.length() - 1);
+	}
+
+	public static List<String> removeBlank(String[] input) {
+		return Stream.of(input).map(String::trim).collect(Collectors.toList());
+	}
+
+	public static String[] splitByComma(String input) {
+		return input.split(",");
+	}
+}
